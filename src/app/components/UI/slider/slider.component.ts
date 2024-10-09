8a31c96713f0aa3e932a771f524e0a9a24005d19
Shiv thakur <shivthakur111@users.noreply.github.com>
2024-10-10 01:35:11 +0545

    First Page Complete

--- src/app/components/UI/slider/slider.component.ts
+++ src/app/components/UI/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -6,39 +6,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 
-export class SliderComponent {
-
-
-  slides = [
+export class SliderComponent implements OnInit {
+  items = [
     {
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/800px-Altja_j%C3%B5gi_Lahemaal.jpg',
-      title: 'CCS Receives Sandhills Region\'s Science of Reading True Trailblazer Award',
-      description: 'CCS Receives Sandhills Region\'s Science of Reading True Trailblazer Award'
+      image: 'https://i.ibb.co/qCkd9jS/img1.jpg',
     },
     {
-      imageUrl: 'https://image.shutterstock.com/image-photo/dawn-mountains-wide-green-meadow-250nw-1717881349.jpg',
-      title: 'Board of Education Committee Meeting',
-      description: 'Cumberland County Board of Education COMMITTEE Meetings Announcement'
+      image: 'https://i.ibb.co/jrRb11q/img2.jpg',
     },
     {
-      imageUrl: 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/c8/Altja_j%C3%B5gi_Lahemaal.jpg/800px-Altja_j%C3%B5gi_Lahemaal.jpg',
-      title: '\'Need to Know\' Update',
-      description: '\'Need to Know\' Update for the Week of September 29, 2024'
+      image: 'https://i.ibb.co/NSwVv8D/img3.jpg',
+    },
+    {
+      image: 'https://i.ibb.co/Bq4Q0M8/img4.jpg',
     }
   ];
 
-  currentIndex = 0;
+  activeIndex = 0;
 
-  getTransform() {
-    return `translateX(-${this.currentIndex * 100}%)`;
+  ngOnInit(): void {
+    this.autoSlide();
   }
 
-  prevSlide() {
-    this.currentIndex = (this.currentIndex === 0) ? this.slides.length - 1 : this.currentIndex - 1;
+  nextSlide(): void {
+    this.activeIndex = (this.activeIndex + 1) % this.items.length;
   }
 
-  nextSlide() {
-    this.currentIndex = (this.currentIndex === this.slides.length - 1) ? 0 : this.currentIndex + 1;
+  prevSlide(): void {
+    this.activeIndex = (this.activeIndex - 1 + this.items.length) % this.items.length;
   }
 
+  autoSlide(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 2000);
+  }
 }
--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -8,28 +8,28 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit{
   events = [
     {
-      title: 'Happy Dashain from Lincoln School!',
+      title: 'Happy Dashain from SRNSS',
       description: 'Wishing our entire Lincoln community and all families across Nepal a joyous and prosperous Dashain...',
-      image: 'https://scontent.fktm18-1.fna.fbcdn.net/v/t39.30808-6/461166572_4091639424492255_1891134706911474238_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEma_rJ-FOXM3YWdVQPwVwZulTA8s-WTou6VMDyz5ZOiw9d70U76g13dWwMYB5CGC0utmIH1QxsgHd9IFj0VuGq&_nc_ohc=GdNsgFLYuX0Q7kNvgGyPNRu&_nc_ht=scontent.fktm18-1.fna&_nc_gid=AdnBaGzZcILq2h1TNarqLH_&oh=00_AYD7SQzjAqX1gL5heTPu51YX2ddCega7cdOXfgEtxWpjaA&oe=670745E1', // Replace with the path to your image
+      image: 'https://www.adventurehimalayacircuit.com/blog/wp-content/uploads/2019/10/Dashain-Festival.jpg', // Replace with the path to your image
       date: '04 October, 2024',
     },
     {
-      title: 'Synchronized Swimming in Grade 9 PE',
-      description: 'With a wonderful display of rhythm, coordination, and grace, Grade 9 students finished their aquatics...',
-      image: 'https://scontent.fktm18-1.fna.fbcdn.net/v/t39.30808-6/461166572_4091639424492255_1891134706911474238_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEma_rJ-FOXM3YWdVQPwVwZulTA8s-WTou6VMDyz5ZOiw9d70U76g13dWwMYB5CGC0utmIH1QxsgHd9IFj0VuGq&_nc_ohc=GdNsgFLYuX0Q7kNvgGyPNRu&_nc_ht=scontent.fktm18-1.fna&_nc_gid=AdnBaGzZcILq2h1TNarqLH_&oh=00_AYD7SQzjAqX1gL5heTPu51YX2ddCega7cdOXfgEtxWpjaA&oe=670745E1',
+      title: 'Happy Dashain from SRNSS',
+      description: 'Wishing our entire Lincoln community and all families across Nepal a joyous and prosperous Dashain...',
+      image: 'https://www.adventurehimalayacircuit.com/blog/wp-content/uploads/2019/10/Dashain-Festival.jpg', // Replace with the path to your image
       date: '04 October, 2024',
     },
     {
-      title: 'Writers’ World',
-      description: 'Writers’ World – the second and fifth-grade classes met to share their completed personal narratives...',
-      image: 'https://scontent.fktm18-1.fna.fbcdn.net/v/t39.30808-6/461166572_4091639424492255_1891134706911474238_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEma_rJ-FOXM3YWdVQPwVwZulTA8s-WTou6VMDyz5ZOiw9d70U76g13dWwMYB5CGC0utmIH1QxsgHd9IFj0VuGq&_nc_ohc=GdNsgFLYuX0Q7kNvgGyPNRu&_nc_ht=scontent.fktm18-1.fna&_nc_gid=AdnBaGzZcILq2h1TNarqLH_&oh=00_AYD7SQzjAqX1gL5heTPu51YX2ddCega7cdOXfgEtxWpjaA&oe=670745E1',
-      date: '03 October, 2024',
+      title: 'Happy Dashain from SRNSS',
+      description: 'Wishing our entire Lincoln community and all families across Nepal a joyous and prosperous Dashain...',
+      image: 'https://www.adventurehimalayacircuit.com/blog/wp-content/uploads/2019/10/Dashain-Festival.jpg', // Replace with the path to your image
+      date: '04 October, 2024',
     },
     {
-      title: 'Grade 2 Target Ball',
-      description: 'Grade 2 students are currently doing a unit on Cooperative Games. Today, they were playing “Target Ball”...',
-      image: 'https://scontent.fktm18-1.fna.fbcdn.net/v/t39.30808-6/461166572_4091639424492255_1891134706911474238_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=833d8c&_nc_eui2=AeEma_rJ-FOXM3YWdVQPwVwZulTA8s-WTou6VMDyz5ZOiw9d70U76g13dWwMYB5CGC0utmIH1QxsgHd9IFj0VuGq&_nc_ohc=GdNsgFLYuX0Q7kNvgGyPNRu&_nc_ht=scontent.fktm18-1.fna&_nc_gid=AdnBaGzZcILq2h1TNarqLH_&oh=00_AYD7SQzjAqX1gL5heTPu51YX2ddCega7cdOXfgEtxWpjaA&oe=670745E1',
-      date: '02 October, 2024',
+      title: 'Happy Dashain from SRNSS',
+      description: 'Wishing our entire Lincoln community and all families across Nepal a joyous and prosperous Dashain...',
+      image: 'https://www.adventurehimalayacircuit.com/blog/wp-content/uploads/2019/10/Dashain-Festival.jpg', // Replace with the path to your image
+      date: '04 October, 2024',
     },
   ];
 
